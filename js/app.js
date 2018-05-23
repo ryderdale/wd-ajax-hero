5ba96f8fe4ed6849e53196d4cdd3504282ee3d5d
Ryder Dale <rydersdale@gmail.com>
2018-05-23 15:34:45 -0500

    to get plot synopsosis to work, will need to query the full api with an actual api key. Then, add an event lisner to all the cards and query the api with the id, that was appended to the div of that card upon the search

--- js/app.js
+++ js/app.js
@@ -14,16 +14,16 @@
 
       $title.attr({
         'data-position': 'top',
-        'data-tooltip': movie.title
+        'data-tooltip': movie.Title
       });
 
-      $title.tooltip({ delay: 50 }).text(movie.title);
+      $title.tooltip({ delay: 50 }).text(movie.Title);
 
       const $poster = $('<img>').addClass('poster');
 
       $poster.attr({
-        src: movie.poster,
-        alt: `${movie.poster} Poster`
+        src: movie.Poster,
+        alt: `${movie.Poster} Poster`
       });
 
       $content.append($title, $poster);
@@ -33,17 +33,18 @@
       const $plot = $('<a>');
 
       $plot.addClass('waves-effect waves-light btn modal-trigger');
-      $plot.attr('href', `#${movie.id}`);
+      $plot.attr('href', `#${movie.imdbID}`);
       $plot.text('Plot Synopsis');
 
       $action.append($plot);
       $card.append($action);
 
-      const $modal = $('<div>').addClass('modal').attr('id', movie.id);
+      const $modal = $('<div>').addClass('modal').attr('id', movie.imdbID
+    );
       const $modalContent = $('<div>').addClass('modal-content');
-      const $modalHeader = $('<h4>').text(movie.title);
-      const $movieYear = $('<h6>').text(`Released in ${movie.year}`);
-      const $modalText = $('<p>').text(movie.plot);
+      const $modalHeader = $('<h4>').text(movie.Title);
+      const $movieYear = $('<h6>').text(`Released in ${movie.Year}`);
+      const $modalText = $('<p>').text(movie.Plot);
 
       $modalContent.append($modalHeader, $movieYear, $modalText);
       $modal.append($modalContent);
@@ -54,7 +55,40 @@
 
       $('.modal-trigger').leanModal();
     }
+    console.log(movies[0]);
   };
 
+ 
+
   // ADD YOUR CODE HERE
+
+const keyURL = "https://omdb-api.now.sh/?s=";
+let searchButton = document.getElementById("search-button");
+searchButton.addEventListener('click', function fetchMovies(event) {
+  event.preventDefault();
+  while(movies.length > 0) {
+    movies.pop();
+}
+  let searchInput = document.getElementById('search').value;
+  let searchString = keyURL + searchInput;
+  let movie;
+  if (searchInput) {
+    fetch(searchString)
+    .then((response) => response.json())
+    .then((data)=> {movie = data;
+      console.log(movie);
+      console.log(movie.length);
+      for (let i = 0; i < movie.Search.length; i++) {
+        movies.push(movie.Search[i]);
+      }
+      // for (let i in movie.Search) {
+        
+      // };
+      console.log(movies);
+      renderMovies()})
+  }
+   
+    
+});
+  
 })();
